ed730829a17354c6ea27b0577f3cab14cac6f348
Miralem Cebic <mail@miralem-cebic.de>
2016-01-31 11:18:41 +0100

    contacts layout, firebase simple setup prepared, form for adding new user

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -1,14 +1,25 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/contacts', {
     templateUrl: 'contacts/contacts.html',
-    controller: 'View1Ctrl'
+    controller: 'ContactsCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('ContactsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
 
+    var ref = new Firebase('https://mycontacts-mcc.firebaseio.com/contacts');
+
+    $scope.contacts = $firebaseArray(ref);
+
+    $scope.showAddForm = function() {
+        $scope.addFormShow = true;
+    };
+
+    $scope.addFormSubmit = function() {
+        $scope.addFormShow = false;
+    };
 }]);
